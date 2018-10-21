a61660a1c348c73457ae09722dad64d15285023b
HAN <HAN@DESKTOP-VO100A4>
2018-10-20 21:51:39 -0500

    Ventanas por bancos

---
+++ src/credito_interfaz/Credito_interfaz.java
@@ -0,0 +1,11 @@
+
+package credito_interfaz;
+
+public class Credito_interfaz {
+
+    public static void main(String[] args) {
+        inicio i=new inicio();
+        i.setVisible(true);
+    }
+    
+}
